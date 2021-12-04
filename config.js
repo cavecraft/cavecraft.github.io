5654a57ba4f3a2c861030a60f495396c221bc3d9
SuperCreeper20 <95530874+SuperCreeper20@users.noreply.github.com>
2021-12-05 00:13:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ var ip_copied = "IP kopiert!";
 var firefly_count = 20;
 
 staff("_Rewe", "32ad3141-93c5-4b2d-91bf-1cde7db6dcdb", "Owner");
-staff("Liebeg", "3d422183-64b1-4bbc-8eea-30b04d18d697", "Buddy");
+staff("Liebeg", "3d422183-64b1-4bbc-8eea-30b04d18d697", "Buddy | ᛖxᛏᚱᛖᛗ ᚷᛖᚠᚨᛖᚺᚱᛚᛁᚲᚺ, ᛊᛞᚠᛞᚱᛏ ᛏᛞᛖᛏᛖᚾ!");
 
 vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
 vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
