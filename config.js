ffa8907b30ad03176d5db3289ae2dd4542c2a48d
SuperCreeper20 <95530874+SuperCreeper20@users.noreply.github.com>
2021-12-04 17:17:32 +0100

    Website created

---
+++ config.js
@@ -0,0 +1,19 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "CaveCraft";
+var server_ip = "cavecraft.mgservers.de";
+var server_port = 25006;
+var ip_copied = "IP kopiert!";
+
+var firefly_count = 20;
+
+staff("_Rewe", "32ad3141-93c5-4b2d-91bf-1cde7db6dcdb", "Owner");
+staff("Liebeg", "3d422183-64b1-4bbc-8eea-30b04d18d697", "Buddy");
+
+vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
+vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
+vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/");
+vote("Top Minecraft Servers", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://topminecraftservers.org/");
