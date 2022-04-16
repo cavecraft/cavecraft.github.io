861954880785ddd4125bccb3201eed9ceb57508e
SuperCreeper20 <95530874+SuperCreeper20@users.noreply.github.com>
2022-04-16 17:25:58 +0200

    Updated

---
+++ js/maintenance.js
@@ -0,0 +1,47 @@
+let style = `
+  <style>
+  .lw-overlay {
+    z-index: 999;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.7);
+  }.lw-overlay:target {
+    visibility: visible;
+    opacity: 1;
+  }.lw-popup {
+    margin: 70px auto;
+    padding: 20px;
+    background: #fff;
+    border-radius: 5px;
+    width: 30%;
+    position: relative;
+    transition: all 2s ease-in-out;
+  }.lw-popup .content {
+    max-height: 30%;
+    overflow: auto;
+  }
+  </style>
+`
+
+let warning = `
+  <div id="popup" class="lw-overlay">
+    <div class="lw-popup">
+      <h2>Warning!</h2>
+      <div class="lw-content">
+        <p>
+          Diese Seite ist zurzeit aus Wartungsgründen nicht verfügbar. Normalerweise sollte eine automatische Weiterleitung erfolgen
+          <br><br>
+          <a href="https://olliejw.me/tos.html">Klick mich</a>
+        </p>
+      </div>
+    </div>
+  </div>
+`
+
+if (window.pageBlocked == true) {
+  $("head").prepend(style)
+  $("body").append(warning)
+}
